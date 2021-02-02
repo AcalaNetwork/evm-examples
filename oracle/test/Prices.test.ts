6aa0a99660174c2d61610f532f2aa769bc1fd6fa
Nantian <nxiiun@gmail.com>
2021-02-02 17:42:15 +1300

    fix test (#3)
    * fix test
    
    * fix
    
    * fix

--- oracle/test/Prices.test.ts
+++ oracle/test/Prices.test.ts
@@ -1,29 +1,14 @@
-import { expect, use } from "chai";
-import { Contract } from "ethers";
-import { deployContract, solidity } from "ethereum-waffle";
+import { Provider, Signer, TestAccountSigningKey } from "@acala-network/bodhi";
 import { evmChai } from "@acala-network/bodhi/evmChai";
-import Prices from "../build/Prices.json";
-import { Wallet, Provider } from "@acala-network/bodhi";
-import { options } from "@acala-network/api";
 import { WsProvider } from "@polkadot/api";
-import { WalletSigningKey } from "@acala-network/bodhi";
 import { createTestPairs } from "@polkadot/keyring/testingPairs";
+import { expect, use } from "chai";
+import { deployContract, solidity } from "ethereum-waffle";
+import { Contract } from "ethers";
+import Prices from "../build/Prices.json";
 
 use(solidity);
 use(evmChai);
-
-const PRIVATES = [
-  "0x3a32660c3aff5b3087c68b071aa278362b2a3df2916551b4744927d105e0718b",
-  "0xb04b26b3f8f30c79ee2d8a37f94e0f14fa4612bee23b448ad446909893ad658f",
-  "0xfcf4d14f0d6278a033843989285cf8b0713db44d0335e877730f34c65d7a655f",
-  "0x8c62fab824288c49319b06ecc3a8d68f4d9efa8497cda294b749b2b04f660052",
-  "0x2e2e3f97cdb4077d6bd7d673e61f9a74fcc79d48b89e41b8ec073d2fae0491a1",
-  "0xf024d03371d94b35aec0024e5a2c01ee421df49aa838f1cc7d1aef2165dfc85d",
-  "0xefc44f8ded2ce7589228d472cf26c99a1da9bcfa72a0dcd04cd459189aaa6a99",
-  "0x5a78d856a6ea9d29d97d90810bdae89c54eee62641ff9876b63c09dc8cbd3634",
-  "0x1aeb6d3e92815a3a190afb624348bca9df15db6415e9122cf89f3e7c1261286c",
-  "0x8430425785e8ad67866029c4ebc1b33bcb9d62ce6d84dfc8507411390baf5985",
-];
 
 const provider = new Provider({
   provider: new WsProvider("ws://127.0.0.1:9944"),
@@ -38,38 +23,40 @@ const getWallets = async () => {
     testPairs.bob,
     testPairs.bob_stash,
   ];
+  const signingKey = new TestAccountSigningKey(provider.api.registry);
+
+  signingKey.addKeyringPair(Object.values(testPairs));
+
   await provider.api.isReady;
 
-  let wallets: any[] = [];
+  let wallets: Signer[] = [];
 
-  for (const [index, pair] of pairs.entries()) {
-    const wallet = new Wallet(
-      provider,
-      pair,
-      new WalletSigningKey(PRIVATES[index])
-    );
+  for (const pair of pairs) {
+    const wallet = new Signer(provider, pair.address, signingKey);
 
-    const isConnected = await wallet.isConnented();
+    const isClaimed = await wallet.isClaimed();
 
-    if (!isConnected) {
-      wallet.claimEvmAccount();
+    if (!isClaimed) {
+      await wallet.claimDefaultAccount();
     }
 
     wallets.push(wallet);
   }
 
-  return wallets as any[];
+  return wallets;
 };
 
 const feedValues = async (token: string, price: number) => {
   return new Promise((resolve) => {
-    provider.api.tx.acalaOracle.feedValues([[{'Token': token}, price]]).signAndSend(testPairs.alice, (result) => {
-      if (result.status.isInBlock) {
-        resolve();
-      }
-    });
+    provider.api.tx.acalaOracle
+      .feedValues([[{ Token: token }, price]])
+      .signAndSend(testPairs.alice.address, (result) => {
+        if (result.status.isInBlock) {
+          resolve(undefined);
+        }
+      });
   });
-}
+};
 
 describe("Prices", () => {
   let prices: Contract;
@@ -80,28 +67,41 @@ describe("Prices", () => {
   });
 
   after(async () => {
-    process.exit(0);
+    provider.api.disconnect()
   });
 
   it("getPrice works", async () => {
-    await feedValues('XBTC', 34_500);
-    expect(await prices.getPrice('0x0000000000000000000000000000000000000803')).to.equal(34_500);
+    await feedValues("XBTC", 34_500);
+    expect(
+      await prices.getPrice("0x0000000000000000000000000000000000000803")
+    ).to.equal(34_500);
 
-    await feedValues('XBTC', 33_800);
-    expect(await prices.getPrice('0x0000000000000000000000000000000000000803')).to.equal(33_800);
+    await feedValues("XBTC", 33_800);
+    expect(
+      await prices.getPrice("0x0000000000000000000000000000000000000803")
+    ).to.equal(33_800);
 
-    await feedValues('DOT', 15);
-    expect(await prices.getPrice('0x0000000000000000000000000000000000000802')).to.equal(15);
+    await feedValues("DOT", 15);
+    expect(
+      await prices.getPrice("0x0000000000000000000000000000000000000802")
+    ).to.equal(15);
 
-    await feedValues('DOT', 16);
-    expect(await prices.getPrice('0x0000000000000000000000000000000000000802')).to.equal(16);
+    await feedValues("DOT", 16);
+    expect(
+      await prices.getPrice("0x0000000000000000000000000000000000000802")
+    ).to.equal(16);
   });
 
-  it('ignores invalid address', async () => {
+  it("ignores invalid address", async () => {
     // system contract addresses start with 12 zero bytes
-    await expect(prices.getPrice('0x0000000000000000000000010000000000000000')).to.be.revertedWith("Oracle: Not a system contract");
-    await expect(prices.getPrice('0x1000000000000000000000000000000000000000')).to.be.revertedWith("Oracle: Not a system contract");
+    await expect(
+      prices.getPrice("0x0000000000000000000000010000000000000000")
+    ).to.be.revertedWith("Oracle: Not a system contract");
+    await expect(
+      prices.getPrice("0x1000000000000000000000000000000000000000")
+    ).to.be.revertedWith("Oracle: Not a system contract");
     // not MultiCurrency token
-    await expect(prices.getPrice('0x0000000000000000000000000000000000000806')).to.be.reverted;
-  })
+    await expect(prices.getPrice("0x0000000000000000000000000000000000000806"))
+      .to.be.reverted;
+  });
 });
