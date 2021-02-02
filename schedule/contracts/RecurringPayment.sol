28df048e31086176c9a88183658e3d049e834ebd
Xiliang Chen <xlchen1291@gmail.com>
2021-02-02 22:37:35 +1300

    scheduler example (#2)
    * scheduler example
    
    * fix
    
    * update
    
    * fix schedule example

---
+++ schedule/contracts/RecurringPayment.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.6.0;
+
+import "./Scheduler.sol";
+
+contract RecurringPayment {
+    uint period;
+    uint remainingCount;
+    uint amount;
+    address payable to;
+    Scheduler constant scheduler = Scheduler(0x0000000000000000000000000000000000000808);
+
+    constructor(uint _period, uint _count, uint _amount, address payable _to) public payable {
+        require(msg.value >= _count * _amount);
+
+        period = _period;
+        remainingCount = _count;
+        amount = _amount;
+        to = _to;
+
+        scheduler.scheduleCall(address(this), 0, 50000, 100, _period, abi.encodeWithSignature("pay()"));
+    }
+
+    function pay() public {
+        require(msg.sender == address(this));
+
+        if (remainingCount == 1) {
+            selfdestruct(to);
+        } else {
+            to.transfer(amount);
+            
+            remainingCount--;
+            scheduler.scheduleCall(address(this), 0, 50000, 100, period, abi.encodeWithSignature("pay()"));
+        }
+    }
+}
--- schedule/test/Schedule.test.ts
+++ schedule/test/Schedule.test.ts
@@ -1,10 +1,11 @@
 import { expect, use } from "chai";
 import { ethers, Contract } from "ethers";
+import { deployContract, solidity } from "ethereum-waffle";
 import { evmChai } from "@acala-network/bodhi/evmChai";
 import { TestAccountSigningKey, Provider, Signer } from "@acala-network/bodhi";
 import { WsProvider } from "@polkadot/api";
 import { createTestPairs } from "@polkadot/keyring/testingPairs";
-
+import RecurringPayment from "../build/RecurringPayment.json";
 
 use(evmChai);
 
@@ -59,7 +60,7 @@ const SCHEDULE_CALL_ABI = [
   "function scheduleCall(address contract_address, uint256 value, uint256 gas_limit, uint256 storage_limit, uint256 min_delay, bytes calldata input_data) public returns (uint256, uint256)",
 ]
 
-const ACA_ERC20_ADDRESS = '0x0000000000000000000000000000000000000800';
+const DOT_ERC20_ADDRESS = '0x0000000000000000000000000000000000000802';
 const ERC20_ABI = [
   // Read-Only Functions
   "function balanceOf(address owner) view returns (uint256)",
@@ -89,16 +90,12 @@ describe("Schedule", () => {
 
   it("ScheduleCall works", async () => {
     const target_block_number = Number(await provider.api.query.system.number()) + 4;
-    console.log("target_block_number", target_block_number);
 
-    const erc20 = new ethers.Contract(ACA_ERC20_ADDRESS, ERC20_ABI, walletTo as any);
+    const erc20 = new ethers.Contract(DOT_ERC20_ADDRESS, ERC20_ABI, walletTo as any);
     const tx = await erc20.populateTransaction.transfer(walletTo.getAddress(), 1_000_000);
     console.log(tx, ethers.utils.hexlify(tx.data as string));
 
-    await schedule.scheduleCall(ACA_ERC20_ADDRESS, 0, 300000, 10000, 1, ethers.utils.hexlify(tx.data as string));
-    //await expect(schedule.scheduleCall(ACA_ERC20_ADDRESS, 0, 300000, 10000, 1, ethers.utils.hexlify(tx.data)))
-    //  .to.emit(schedule, "LocalScheduledCall")
-    //  .withArgs(wallet.address, ACA_ERC20_ADDRESS, target_block_number, 0);
+    await schedule.scheduleCall(DOT_ERC20_ADDRESS, 0, 300000, 10000, 1, ethers.utils.hexlify(tx.data as string));
 
     let current_block_number = Number(await provider.api.query.system.number());
     let balance = await erc20.balanceOf(await walletTo.getAddress());
@@ -108,6 +105,41 @@ describe("Schedule", () => {
     }
 
     let new_balance = await erc20.balanceOf(await walletTo.getAddress());
-    expect(new_balance.eq(balance.add(1_000_000))).to.be.ok;
+    expect(new_balance.toString()).to.equal(balance.add(1_000_000).toString());
+  });
+
+  it("works with RecurringPayment", async () => {
+    const transferTo = await ethers.Wallet.createRandom().getAddress();
+    const inital_block_number = Number(await provider.api.query.system.number());
+
+    const recurringPayment = await deployContract(wallet as any, RecurringPayment, [3, 4, 1000, transferTo], { value: 5000, gasLimit: 2_000_000 });
+
+    expect((await provider.getBalance(transferTo)).toNumber()).to.equal(0);
+
+    let current_block_number = Number(await provider.api.query.system.number());
+
+    while (current_block_number < (inital_block_number + 5)) {
+      await next_block(current_block_number);
+      current_block_number = Number(await provider.api.query.system.number());
+    }
+
+    expect((await provider.getBalance(transferTo)).toNumber()).to.equal(1000);
+
+    current_block_number = Number(await provider.api.query.system.number());
+    while (current_block_number < (inital_block_number + 14)) {
+      await next_block(current_block_number);
+      current_block_number = Number(await provider.api.query.system.number());
+    }
+
+    expect((await provider.getBalance(transferTo)).toNumber()).to.equal(3000);
+
+    current_block_number = Number(await provider.api.query.system.number());
+    while (current_block_number < (inital_block_number + 17)) {
+      await next_block(current_block_number);
+      current_block_number = Number(await provider.api.query.system.number());
+    }
+
+    expect((await provider.getBalance(recurringPayment.address)).toNumber()).to.equal(0);
+    expect((await provider.getBalance(transferTo)).toNumber()).to.equal(5000);
   });
 });
