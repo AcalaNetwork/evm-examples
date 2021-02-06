6c33b1136644ae0d1f95b258636784ef8fd40518
Bryan Chen <xlchen1291@gmail.com>
2021-02-06 23:58:40 +1300

    update example

--- oracle/contracts/IOracle.sol
+++ oracle/contracts/IOracle.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.6.0;
 
 interface IOracle {
+    // Get the price of the currency_id.
+    // Returns the (price, timestamp)
     function getPrice(address token) external view returns (uint256, uint256);
 }
