2ce6a8fcf0e315f353083673c7f54844637cfb47
zjb0807 <zjb0807@qq.com>
2021-02-17 23:23:53 +0800

    add reschedule and cancel (#5)

--- scheduler/test/Scheduler.test.ts
+++ scheduler/test/Scheduler.test.ts
@@ -57,7 +57,13 @@ const next_block = async (block_number: number) => {
 
 const SCHEDULE_CALL_ADDRESS = '0x0000000000000000000000000000000000000808';
 const SCHEDULE_CALL_ABI = [
-  "function scheduleCall(address contract_address, uint256 value, uint256 gas_limit, uint256 storage_limit, uint256 min_delay, bytes calldata input_data) public returns (uint256, uint256)",
+  "function scheduleCall(address contract_address, uint256 value, uint256 gas_limit, uint256 storage_limit, uint256 min_delay, bytes memory input_data) public returns (bool)",
+  "function cancelCall(bytes memory task_id) public returns (bool)",
+  "function rescheduleCall(uint256 min_delay, bytes memory task_id) public returns (bool)",
+
+   "event ScheduledCall(address indexed sender, address indexed contract_address, bytes task_id)",
+   "event CanceledCall(address indexed sender, bytes task_id)",
+   "event RescheduledCall(address indexed sender, bytes task_id)",
 ]
 
 const DOT_ERC20_ADDRESS = '0x0000000000000000000000000000000000000802';
@@ -106,6 +112,59 @@ describe("Schedule", () => {
 
     let new_balance = await erc20.balanceOf(await walletTo.getAddress());
     expect(new_balance.toString()).to.equal(balance.add(1_000_000).toString());
+  });
+
+  it("CancelCall works", async () => {
+    const erc20 = new ethers.Contract(DOT_ERC20_ADDRESS, ERC20_ABI, walletTo as any);
+    const tx = await erc20.populateTransaction.transfer(walletTo.getAddress(), 1_000_000);
+    console.log(tx, ethers.utils.hexlify(tx.data as string));
+
+    let iface = new ethers.utils.Interface(SCHEDULE_CALL_ABI);
+
+    let current_block_number = Number(await provider.api.query.system.number());
+    await schedule.scheduleCall(DOT_ERC20_ADDRESS, 0, 300000, 10000, 2, ethers.utils.hexlify(tx.data as string));
+
+    let block_hash = await provider.api.query.system.blockHash(current_block_number);
+    const data = await provider.api.derive.tx.events(block_hash);
+    //let event = data.events.filter(item => item.event.data.some(data => data.address == SCHEDULE_CALL_ADDRESS));
+    let event = data.events.filter(item => item.event.data.some(data => data.address == SCHEDULE_CALL_ADDRESS && data.topics[0]=='0xf50ab0aa329811f23150e5490fc00ea0baf136a55280b7e88703b4753d4097ce'));
+    console.log("event:", event.toString());
+    if (event.length > 0) {
+      let log = {
+        topics: [event[0].event.data[0].topics[0].toString(), event[0].event.data[0].topics[1].toString(), event[0].event.data[0].topics[2].toString()], data: event[0].event.data[0].data.toString()
+      };
+      let decode_log = await iface.parseLog(log);
+      console.log("task_id:" + decode_log.args.task_id);
+      await schedule.cancelCall(ethers.utils.hexlify(decode_log.args.task_id));
+    } else {
+      expect(false).to.not.be.ok;
+    }
+  });
+
+  it("RescheduleCall works", async () => {
+    const erc20 = new ethers.Contract(DOT_ERC20_ADDRESS, ERC20_ABI, walletTo as any);
+    const tx = await erc20.populateTransaction.transfer(walletTo.getAddress(), 1_000_000);
+    console.log(tx, ethers.utils.hexlify(tx.data as string));
+
+    let iface = new ethers.utils.Interface(SCHEDULE_CALL_ABI);
+
+    let current_block_number = Number(await provider.api.query.system.number());
+    await schedule.scheduleCall(DOT_ERC20_ADDRESS, 0, 300000, 10000, 4, ethers.utils.hexlify(tx.data as string));
+
+    let block_hash = await provider.api.query.system.blockHash(current_block_number);
+    const data = await provider.api.derive.tx.events(block_hash);
+    let event = data.events.filter(item => item.event.data.some(data => data.address == SCHEDULE_CALL_ADDRESS && data.topics[0]=='0xf50ab0aa329811f23150e5490fc00ea0baf136a55280b7e88703b4753d4097ce'));
+    console.log("event:", event.toString());
+    if (event.length > 0) {
+      let log = {
+        topics: [event[0].event.data[0].topics[0].toString(), event[0].event.data[0].topics[1].toString(), event[0].event.data[0].topics[2].toString()], data: event[0].event.data[0].data.toString()
+      };
+      let decode_log = await iface.parseLog(log);
+      console.log("task_id:" + decode_log.args.task_id);
+      await schedule.rescheduleCall(5, ethers.utils.hexlify(decode_log.args.task_id));
+    } else {
+      expect(false).to.not.be.ok;
+    }
   });
 
   it("works with RecurringPayment", async () => {
