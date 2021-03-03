9f7e86da63a2cc33765c1e931039d64ca9aabc46
zjb0807 <zjb0807@qq.com>
2021-03-03 11:16:13 +0800

    update acala-network/contracts

--- oracle/contracts/Prices.sol
+++ oracle/contracts/Prices.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
 import "@acala-network/contracts/oracle/IOracle.sol";
 import "@acala-network/contracts/utils/Address.sol";
