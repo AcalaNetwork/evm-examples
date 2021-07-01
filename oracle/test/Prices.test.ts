5710bf0592ba9cda4490c0ba0f8b249cde2d43f6
zjb0807 <zjb0807@qq.com>
2021-07-02 07:46:26 +0800

    revert oracle

--- oracle/test/Prices.test.ts
+++ oracle/test/Prices.test.ts
@@ -12,8 +12,7 @@ use(solidity);
 use(evmChai);
 
 const provider = new TestProvider({
-provider: new WsProvider("ws://127.0.0.1:9944"),
-//provider: new WsProvider("wss://acala-mandala.api.onfinality.io/public-ws"),
+  provider: new WsProvider("ws://127.0.0.1:9944"),
 });
 
 const testPairs = createTestPairs();
@@ -43,8 +42,42 @@ describe("Prices", () => {
   });
 
   it("getPrice works", async () => {
+      await feedValues("RENBTC", BigNumber.from(34_500).mul(BigNumber.from(10).pow(18)).toString());
+      expect(
+        await prices.getPrice(ADDRESS.RENBTC)
+      ).to.equal(BigNumber.from(34_500).mul(BigNumber.from(10).pow(18)).toString());
+
+      await feedValues("RENBTC", BigNumber.from(33_800).mul(BigNumber.from(10).pow(18)).toString());
+      expect(
+        await prices.getPrice(ADDRESS.RENBTC)
+      ).to.equal(BigNumber.from(33_800).mul(BigNumber.from(10).pow(18)).toString());
+
+      await feedValues("DOT", BigNumber.from(15).mul(BigNumber.from(10).pow(18)).toString());
+      expect(
+        await prices.getPrice(ADDRESS.DOT)
+      ).to.equal(BigNumber.from(15).mul(BigNumber.from(10).pow(18)).toString());
+
+      await feedValues("DOT", BigNumber.from(16).mul(BigNumber.from(10).pow(18)).toString());
+      expect(
+        await prices.getPrice(ADDRESS.DOT)
+      ).to.equal(BigNumber.from(16).mul(BigNumber.from(10).pow(18)).toString());
+
       expect(
         await prices.getPrice(ADDRESS.AUSD)
       ).to.equal(BigNumber.from(1).mul(BigNumber.from(10).pow(18)).toString());
+
+      expect(
+        await prices.getPrice(ADDRESS.KUSD)
+      ).to.equal(0);
+  });
+
+  it("ignores invalid address", async () => {
+    // not system contract
+    await expect(
+      prices.getPrice("0x1000000000000000000000000000000000000000")
+    ).to.be.reverted;
+    // not MultiCurrency token
+    await expect(prices.getPrice("0x0000000000000000000000000000000000000000"))
+      .to.be.reverted;
   });
 });
