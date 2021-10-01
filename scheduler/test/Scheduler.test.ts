08b715e068a61ac4921ec36fb8e2c03cc7158074
zjb0807 <zjb0807@qq.com>
2021-10-01 16:07:53 +0800

    update dependencies & improve scheduler

--- scheduler/test/Scheduler.test.ts
+++ scheduler/test/Scheduler.test.ts
@@ -76,21 +76,15 @@ describe("Schedule", () => {
     let current_block_number = Number(await provider.api.query.system.number());
     await schedule.scheduleCall(ADDRESS.DOT, 0, 300000, 10000, 2, ethers.utils.hexlify(tx.data as string));
 
-    let block_hash = await provider.api.query.system.blockHash(current_block_number);
+    let block_hash = await provider.api.rpc.chain.getBlockHash(current_block_number + 1);
     const data = await provider.api.derive.tx.events(block_hash);
-    //let event = data.events.filter(item => item.event.data.some(data => data.address == ADDRESS.Schedule));
-    let event = data.events.filter(item => item.event.data.some(data => data.address == ADDRESS.Schedule && data.topics[0] == iface.getEventTopic(iface.getEvent("ScheduledCall"))));
-    console.log("event:", event.toString());
-    if (event.length > 0) {
-      let log = {
-        topics: [event[0].event.data[0].topics[0].toString(), event[0].event.data[0].topics[1].toString(), event[0].event.data[0].topics[2].toString()], data: event[0].event.data[0].data.toString()
-      };
-      let decode_log = await iface.parseLog(log);
-      console.log("task_id:" + decode_log.args.task_id);
-      await schedule.cancelCall(ethers.utils.hexlify(decode_log.args.task_id));
-    } else {
-      expect(false).to.not.be.ok;
-    }
+
+    let event = data.events.filter(item => provider.api.events.evm.Log.is(item.event));
+    expect(event.length).to.above(0);
+
+    let decode_log = await iface.parseLog(event[0].event.data.toJSON()[0]);
+    console.log("task_id:" + decode_log.args.task_id);
+    await schedule.cancelCall(ethers.utils.hexlify(decode_log.args.task_id));
   });
 
   it("RescheduleCall works", async () => {
@@ -103,20 +97,15 @@ describe("Schedule", () => {
     let current_block_number = Number(await provider.api.query.system.number());
     await schedule.scheduleCall(ADDRESS.DOT, 0, 300000, 10000, 4, ethers.utils.hexlify(tx.data as string));
 
-    let block_hash = await provider.api.query.system.blockHash(current_block_number);
+    let block_hash = await provider.api.rpc.chain.getBlockHash(current_block_number + 1);
     const data = await provider.api.derive.tx.events(block_hash);
-    let event = data.events.filter(item => item.event.data.some(data => data.address == ADDRESS.Schedule && data.topics[0]== iface.getEventTopic(iface.getEvent("ScheduledCall"))));
-    console.log("event:", event.toString());
-    if (event.length > 0) {
-      let log = {
-        topics: [event[0].event.data[0].topics[0].toString(), event[0].event.data[0].topics[1].toString(), event[0].event.data[0].topics[2].toString()], data: event[0].event.data[0].data.toString()
-      };
-      let decode_log = await iface.parseLog(log);
-      console.log("task_id:" + decode_log.args.task_id);
-      await schedule.rescheduleCall(5, ethers.utils.hexlify(decode_log.args.task_id));
-    } else {
-      expect(false).to.not.be.ok;
-    }
+
+    let event = data.events.filter(item => provider.api.events.evm.Log.is(item.event));
+    expect(event.length).to.above(0);
+
+    let decode_log = await iface.parseLog(event[0].event.data.toJSON()[0]);
+    console.log("task_id:" + decode_log.args.task_id);
+    await schedule.rescheduleCall(5, ethers.utils.hexlify(decode_log.args.task_id));
   });
 
   it("works with RecurringPayment", async () => {
@@ -128,7 +117,7 @@ describe("Schedule", () => {
     const inital_block_number = Number(await provider.api.query.system.number());
     await recurringPayment.initialize();
 
-    //expect((await provider.getBalance(transferTo)).toString()).to.equal("0");
+    expect((await provider.getBalance(transferTo)).toString()).to.equal("0");
     expect((await erc20.balanceOf(transferTo)).toString()).to.equal("0");
 
     let current_block_number = Number(await provider.api.query.system.number());
@@ -138,7 +127,7 @@ describe("Schedule", () => {
       current_block_number = Number(await provider.api.query.system.number());
     }
 
-    //expect((await provider.getBalance(transferTo)).toString()).to.equal(dollar.mul(1000).toString());
+    expect((await provider.getBalance(transferTo)).toString()).to.equal(dollar.mul(1000).toString());
     expect((await erc20.balanceOf(transferTo)).toString()).to.equal(dollar.mul(1000).toString());
 
     current_block_number = Number(await provider.api.query.system.number());
@@ -147,7 +136,7 @@ describe("Schedule", () => {
       current_block_number = Number(await provider.api.query.system.number());
     }
 
-    //expect((await provider.getBalance(transferTo)).toString()).to.equal(dollar.mul(3000).toString());
+    expect((await provider.getBalance(transferTo)).toString()).to.equal(dollar.mul(3000).toString());
     expect((await erc20.balanceOf(transferTo)).toString()).to.equal(dollar.mul(3000).toString());
 
     current_block_number = Number(await provider.api.query.system.number());
@@ -156,12 +145,13 @@ describe("Schedule", () => {
       current_block_number = Number(await provider.api.query.system.number());
     }
 
-    //expect((await provider.getBalance(recurringPayment.address)).toString()).to.equal("0");
-    //expect((await provider.getBalance(transferTo)).toString()).to.equal(dollar.mul(5000).toString());
+    expect((await provider.getBalance(recurringPayment.address)).toString()).to.equal("0");
     expect((await erc20.balanceOf(recurringPayment.address)).toNumber()).to.equal(0);
     if (!process.argv.includes("--with-ethereum-compatibility")) {
+        expect((await provider.getBalance(transferTo)).toString()).to.equal("49999970797782360");
         expect((await erc20.balanceOf(transferTo)).toString()).to.equal("49999970797782360");
     } else {
+        expect((await provider.getBalance(transferTo)).toString()).to.equal(dollar.mul(5000).toString());
         expect((await erc20.balanceOf(transferTo)).toString()).to.equal(dollar.mul(5000).toString());
     }
 
